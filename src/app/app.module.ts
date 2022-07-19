752a4c5815f8302c8812910781e4457510148a43
jucelio coelho <juceliocoelho2010@gmail.com>
2022-07-19 14:30:34 -0300

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
 import { MultiplicarPorPipe } from './multiplicar-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiroComponenteComponent,
     SegundoComponenteComponent,
-    MultiplicarPorPipe
+    MultiplicarPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
