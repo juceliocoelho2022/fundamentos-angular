bd2167248a841a0398645b07ca206a4d89de3454
jucelio coelho <juceliocoelho2010@gmail.com>
2022-07-19 16:29:25 -0300

    COMPONENTES

---
+++ src/app/renderizando-listas/renderizando-listas.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Celular } from '../types/Celular';
+
+@Component({
+  selector: 'app-renderizando-listas',
+  templateUrl: './renderizando-listas.component.html',
+  styleUrls: ['./renderizando-listas.component.css']
+})
+export class RenderizandoListasComponent  {
+
+  celulares: Celular[] = [
+    {id: 1, nome: "Celular xl", descricao: "Um celular grande", esgotado :false},
+    {id: 2, nome: "Celular normal", esgotado :false},
+    {id: 3, nome: "Celular mini", descricao: "Um celular pequeno", esgotado :true},
+
+  ]
+
+}
